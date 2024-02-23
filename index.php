bc29c4a4811ff29593a1656932090d9f88af1b05
Nevec02 <nachocda@gmail.com>
2024-02-23 10:42:09 +0100

    login admin

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Rincón FrutiCola de Kike</title>
-    <link rel="stylesheet" href="./styles/style.css" />
+    <link rel="stylesheet" href="styles/style.css" />
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
   </head>
