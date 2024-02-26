705410b0cc8ce306ac0f18f9bdeda4ce9783d023
Enrique Fdez-Campoamor <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2024-02-26 10:24:42 +0100

    ...

--- includes/config/database.php
+++ includes/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 function conectarDB(){
-    $db = mysqli_connect('localhost', 'root', '', 'fruteria');
+    $db = mysqli_connect('localhost', 'root', 'Thrasher87?', 'fruteria', 3307);
 
     if (!$db){
         echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
