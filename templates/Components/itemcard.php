54cf4bbe8a7b3b891bab1cb264dadb0be1c33245
Enrique Fdez-Campoamor <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2024-03-06 13:14:38 +0100

    updated

--- templates/Components/itemcard.php
+++ templates/Components/itemcard.php
@@ -14,19 +14,21 @@ $result = mysqli_query($conn, $query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product List</title>
+    <title>Productos</title>
 </head>
 <body>
-    <h2>Product List</h2>
+    <h2>Productos</h2>
     <form action="pedido.php" method="post">
         <table>
             <tr>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Quantity</th>
+                <th>Imagen</th>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
             </tr>
             <?php while($row = mysqli_fetch_assoc($result)) { ?>
                 <tr>
+                    <td><?php echo $row['imagen'] ?></td>
                     <td><?php echo $row['nombre']; ?></td>
                     <td><?php echo $row['precio']; ?></td>
                     <td><input type="number" name="quantity[<?php echo $row['id']; ?>]" value="0" min="0"></td>
