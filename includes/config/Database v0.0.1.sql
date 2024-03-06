bed4c5c9fbc7ae89c923714072b5f2ee6f7f06a6
Kike <enriquefernandez-campoamorfernandez@alumnos.tuniversformacion.es>
2024-03-06 15:58:33 +0100

    añadido use database ( Hay que añadir un create)

--- includes/config/Database v0.0.1.sql
+++ includes/config/Database v0.0.1.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `order_details`
 --
 
+USE fruteria;
+
 DROP TABLE IF EXISTS `order_details`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
